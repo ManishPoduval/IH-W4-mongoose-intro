ed02adfe33b482ad63ad1623710867cde853283c
Manish Poduval <manishpoduval@hotmail.com>
2020-05-28 17:26:52 +0530

    mongoose intro

---
+++ app.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('Yayyy Database is connected');
+    })
+    .catch(() => {
+        console.log('Something went wrong!');
+    })
+
+
+let Student = mongoose.model('student', {name: String});
+
+Student.create({name: 'Manish'});
